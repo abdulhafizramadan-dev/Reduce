446fd10b339bd357156c139e86288b7976919bfc
abdulhafizramadan-dev <abdulhafizramadan.dev@gmail.com>
2023-03-01 14:50:50 +0700

    | Create AuthTitle.kt

---
+++ app/src/main/java/com/ahr/reduce/ui/component/auth/AuthTitle.kt
@@ -0,0 +1,46 @@
+package com.ahr.reduce.ui.component.auth
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ahr.reduce.R
+import com.ahr.reduce.ui.theme.ReduceTheme
+
+@Composable
+fun AuthTitle(
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = stringResource(id = text),
+        style = MaterialTheme.typography.titleMedium.copy(
+            fontWeight = FontWeight.SemiBold,
+            lineHeight = 24.sp
+        ),
+        textAlign = TextAlign.Center,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAuthTitle() {
+    ReduceTheme {
+        AuthTitle(
+            text = R.string.register_title,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+    }
+}
