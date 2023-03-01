a635efcf498c7c333a5c56ac6a71293877831a2c
abdulhafizramadan-dev <abdulhafizramadan.dev@gmail.com>
2023-03-01 14:51:25 +0700

    | Create AuthSubtitle.kt

---
+++ app/src/main/java/com/ahr/reduce/ui/component/auth/AuthSubtitle.kt
@@ -0,0 +1,46 @@
+package com.ahr.reduce.ui.component.auth
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ahr.reduce.R
+import com.ahr.reduce.ui.theme.Gray20
+import com.ahr.reduce.ui.theme.ReduceTheme
+
+@Composable
+fun AuthSubtitle(
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = stringResource(id = text),
+        style = MaterialTheme.typography.bodyMedium.copy(
+            lineHeight = 21.sp,
+            color = Gray20,
+        ),
+        textAlign = TextAlign.Center,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAuthSubtitle() {
+    ReduceTheme {
+        AuthSubtitle(
+            text = R.string.register_subtitle,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+    }
+}
