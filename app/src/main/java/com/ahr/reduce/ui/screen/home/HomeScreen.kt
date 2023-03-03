7c15c13467be88397939811f9aeb18e2df49a9e0
abdulhafizramadan-dev <abdulhafizramadan.dev@gmail.com>
2023-03-03 08:31:07 +0700

    | Create HomeScreen.kt

--- app/src/main/java/com/ahr/reduce/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/ahr/reduce/ui/screen/home/HomeScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ahr.reduce.ui.component.product.ProductItem
-import com.ahr.reduce.ui.component.textfield.ReduceSearchOutlinedTextField
 import com.ahr.reduce.ui.data.products
 import com.ahr.reduce.ui.theme.ReduceTheme
 
@@ -38,9 +37,11 @@ fun HomeScreen(
         modifier = modifier.fillMaxSize(),
     ) {
         item(span = { GridItemSpan(maxLineSpan) }) {
-            ReduceSearchOutlinedTextField(
-                query = searchQuery,
-                onQueryChanged = { searchQuery = it }
+            HomeTopAppBar(
+                searchQuery = searchQuery,
+                onSearchQueryChanged = { searchQuery = it },
+                onCartClicked = { },
+                modifier = Modifier.fillMaxWidth()
             )
         }
 
---
+++ app/src/main/java/com/ahr/reduce/ui/screen/home/HomeTopAppBar.kt
@@ -0,0 +1,53 @@
+package com.ahr.reduce.ui.screen.home
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.ahr.reduce.R
+import com.ahr.reduce.ui.component.textfield.ReduceSearchOutlinedTextField
+import com.ahr.reduce.ui.theme.ReduceTheme
+
+@Composable
+fun HomeTopAppBar(
+    searchQuery: String,
+    onSearchQueryChanged: (String) -> Unit,
+    onCartClicked: ()-> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ReduceSearchOutlinedTextField(
+            query = searchQuery,
+            onQueryChanged = onSearchQueryChanged,
+            modifier = Modifier.weight(1f)
+        )
+        IconButton(onClick = onCartClicked) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_shopping_cart),
+                contentDescription = stringResource(R.string.to_cart_screen),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewHomeTopAppBar() {
+    ReduceTheme {
+        HomeTopAppBar(
+            searchQuery = "",
+            onSearchQueryChanged = {},
+            onCartClicked = {},
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
